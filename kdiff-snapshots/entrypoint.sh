e5bf3b9fa4eab3a445dd76effeab3d1674e8be3d
Oguzhan Yilmaz <oguzhan@hepapi.com>
2025-06-29 21:21:52 +0200

    Add Dockerfile and entrypoint script for kdiff snapshots

---
+++ kdiff-snapshots/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the current date and time
+DATE=$(date +%Y-%m-%d)
+
+# Get the current time
+TIME=$(date +%H-%M-%S)
+
+# Get the current user
+USER=$(whoami)
+
+echo "Date: $DATE"
+echo "Time: $TIME"
+echo "User: $USER"
